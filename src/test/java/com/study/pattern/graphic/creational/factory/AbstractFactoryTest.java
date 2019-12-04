1e87d8c4af09c28c2afae76c7637057289897428
Sky <sky_bin@kingdee.com>
2019-12-04 10:53:57 +0800

    Update abstract factory test class

--- src/test/java/com/study/pattern/graphic/creational/factory/AbstractFactoryTest.java
+++ src/test/java/com/study/pattern/graphic/creational/factory/AbstractFactoryTest.java
@@ -15,12 +15,14 @@ public class AbstractFactoryTest {
 
     @Test
     public void testAbstractFactory() {
+        /* 工厂生成系列1 的 A 和 B 产品 */
         AbstractFactory abstractFactory1 = new ConcreteFactory1();
         AbstractProductA abstractProductA1 = abstractFactory1.createProductA();
         AbstractProductB abstractProductB1 = abstractFactory1.createProductB();
         abstractProductA1.use();
         abstractProductB1.eat();
 
+        /* 工厂生成系列2 的 A 和 B 产品 */
         AbstractFactory abstractFactory2 = new ConcreteFactory2();
         AbstractProductA abstractProductA2 = abstractFactory2.createProductA();
         AbstractProductB abstractProductB2 = abstractFactory2.createProductB();
